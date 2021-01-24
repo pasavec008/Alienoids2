512f21de12cbc1b08ea3779c5cbb8ca6a0166102
pasavec008 <m.adamovic999@gmail.com>
2021-01-24 22:27:30 +0100

    Movement of ship

--- js/PlayerShip.js
+++ js/PlayerShip.js
@@ -1,14 +1,15 @@
 class PlayerShip{
     x = screen.width / 2;
     y = screen.height / 2;
-    xSize = 50;
-    ySize = 150;
+    xSize = screen.width / 1920 * 50;
+    ySize = screen.height / 1080 * 50;
     dx = 0;
     dy = 0;
     rotation = 0;
     rotationRadians;
     rotationSpeed = 5;
-    speed = 0.1;
+    speed = 0.2;
+    passiveSlowSpeed = 50;
     health;
     shield;
     texture = new Image();
@@ -18,20 +19,39 @@ class PlayerShip{
     }
 
     change(controller, model){
+        //passive slow
+        if(this.dx > 0)
+            this.dx -= this.dx/this.passiveSlowSpeed;
+        else if(this.dx < 0)
+            this.dx -= this.dx/this.passiveSlowSpeed;
+        if(this.dy > 0)
+            this.dy -= this.dy/this.passiveSlowSpeed;
+        else if(this.dy < 0)
+            this.dy -= this.dy/this.passiveSlowSpeed;
         
         if(controller.keys[37]) //left
             this.rotation -= this.rotationSpeed;
         if(controller.keys[39]) //right
             this.rotation += this.rotationSpeed;
-        
+        if(this.rotation > 360)
+            this.rotation -= 360;
+        else if(this.rotation < 0)
+            this.rotation += 360;
+
         if(controller.keys[38]){//up
             this.rotationRadians = this.rotation * Math.PI / 180.0;
             this.dy -= Math.cos(this.rotationRadians) * this.speed;
             this.dx += Math.sin(this.rotationRadians) * this.speed;
-        } 
+        }
 
         this.x += this.dx;
         this.y += this.dy;
+
+        //edges of map
+        if(this.x < 0 || this.x + this.xSize > screen.width)
+            this.dx *= -1.25;
+        if(this.y < 0 || this.y + this.ySize > screen.height)
+            this.dy *= -1.25;
     }
 
     draw(context){
